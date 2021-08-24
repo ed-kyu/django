df21b7ca0594ecee65c8b385d7d86004cddd6472
ed-kyu <seungkyukim.dev@gmail.com>
2021-08-24 23:59:14 +0900

    feat: Make question and choice models

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -89,8 +89,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',  # 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'db.sqlite3',  # 'db.sqlite3','db.postgresql',
     }
 }
 
