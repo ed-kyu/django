993c15e551fc879275ebd901a913cc77aa088c18
ed-kyu <seungkyukim.dev@gmail.com>
2021-09-02 15:03:25 +0900

    feat: Make index page

--- django-crud/CRUD/articles/views.py
+++ django-crud/CRUD/articles/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'articles/index.html')
+
+
+def new(request):
+    pass
+
+
+def create(request):
+    pass
+
+
+def detail(request, pk):
+    pass
+
+
+def edit(request, pk):
+    pass
+
+
+def update(request, pk):
+    pass
+
+
+def delete(request, pk):
+    pass
