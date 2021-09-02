52aba2af5caad75a26e813ed6bd441e2f02cede6
ed-kyu <seungkyukim.dev@gmail.com>
2021-09-02 23:45:25 +0900

    feat: Make new func

--- django-crud/CRUD/articles/views.py
+++ django-crud/CRUD/articles/views.py
@@ -1,20 +1,33 @@
-from django.shortcuts import render
+from articles.models import Article
+from django.shortcuts import redirect, render
 
 # Create your views here.
 def index(request):
-    return render(request, 'articles/index.html')
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html', context)
 
 
 def new(request):
-    pass
+    return render(request, 'articles/new.html')
 
 
 def create(request):
-    pass
-
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    article = Article(title=title, content=content)
+    article.save()
+    return redirect('articles:detail', article.pk)
+    
 
 def detail(request, pk):
-    pass
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/detail.html', context)
 
 
 def edit(request, pk):
