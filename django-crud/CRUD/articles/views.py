2b26064c22e57b5f596964753f7adc5bc91727f5
ed-kyu <seungkyukim.dev@gmail.com>
2021-09-07 14:51:49 +0900

    feat: Use ModelForm

--- django-crud/CRUD/articles/views.py
+++ django-crud/CRUD/articles/views.py
@@ -1,55 +1,69 @@
-from articles.models import Article
-from django.shortcuts import redirect, render
-
+from .models import Article
+from django.shortcuts import redirect, render, get_object_or_404
+from django.views.decorators.http import require_safe, require_POST, require_http_methods
+from .forms import ArticleForm
 # Create your views here.
+
+@require_http_methods(['GET', 'POST'])
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save()
+            redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'articles/form.html', context)
+    
+
+
+@require_http_methods(['GET', 'SAFE'])
 def index(request):
-    articles = Article.objects.all()
+    articles = Article.objects.order_by('-pk')
     context = {
         'articles': articles,
     }
     return render(request, 'articles/index.html', context)
 
 
-def new(request):
-    return render(request, 'articles/new.html')
-
-
-def create(request):
-    title = request.POST.get('title')
-    content = request.POST.get('content')
-    article = Article(title=title, content=content)
-    article.save()
-    return redirect('articles:detail', article.pk)
-    
-
+@require_safe
 def detail(request, pk):
-    article = Article.objects.get(pk=pk)
+    # article = Article.objects.get(pk=pk)
+    # context = {
+    #     'article': article,
+    # }
+    # return render(request, 'articles/detail.html', context)
+    article = get_object_or_404(Article, pk=pk)
     context = {
         'article': article,
     }
     return render(request, 'articles/detail.html', context)
 
 
-def edit(request, pk):
-    article = Article.objects.get(pk=pk)
-    article.title = request.POST.get('title')
-    article.content = request.POST.get('content')
-    article.save()
-    return redirect('articles:detail', article.pk)
-
-
+@require_http_methods(['GET', 'POST'])
 def update(request, pk):
-    article = Article.objects.get(pk=pk)
+    article = get_object_or_404(Article, pk=pk)
+
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            article = form.save()
+            redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm(instance=article)
+
     context = {
         'article': article,
+        'form': form,
     }
-    return render(request, 'articles/edit.html', context)
+    return render(request, 'articles/form.html', context)
 
 
+@require_POST
 def delete(request, pk):
-    article = Article.objects.get(pk=pk)
-    if request.method == "POST":
-        article.delete()
-        return redirect('articles:index')
-    else:
-        return redirect('articles:detail', article.pk)
+    article = get_object_or_404(Article, pk=pk)
+    article.delete()
+    return redirect('articles:index')
