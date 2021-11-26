95733b3cbd86d4f0d87be565d8eb81a7340f9bca
ed-kyu <seungkyukim.dev@gmail.com>
2021-11-26 13:59:19 +0900

    feat: Change keys

--- movie-django-back/movie/settings.py
+++ movie-django-back/movie/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ay(cqh^m06l29voiy_s7lucs_m3ygk^5ulo$28(w6raiim_x(v'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
